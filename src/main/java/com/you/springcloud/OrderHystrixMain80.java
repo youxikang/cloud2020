eb93e0885d5c8df1e8a852b0e2e08decca0aa6fd
itcast <hello@itcast.cn>
2020-08-19 09:01:22 +0800

    第一次：推送yml文件

---
+++ src/main/java/com/you/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,19 @@
+package com.you.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author shkstart
+ * @date 2020/08/15 -14:19
+ */
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
---
+++ src/main/java/com/you/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,23 @@
+package com.you.springcloud.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author shkstart
+ * @date 2020/08/15 -14:25
+ */
+@Component
+public class PaymentFallbackService implements PaymentHystrixService
+{
+    @Override
+    public String paymentInfo_OK(Integer id)
+    {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK ,o(╥﹏╥)o";
+    }
+
+    @Override
+    public String paymentInfo_TimeOut(Integer id)
+    {
+        return "-----PaymentFallbackService fall back-paymentInfo_TimeOut ,o(╥﹏╥)o";
+    }
+}
